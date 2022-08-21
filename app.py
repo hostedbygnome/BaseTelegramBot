41f26dec77378f116840df6a49f450dd61f7b9f8
Maksim.Novozhilov <enmotioneee@gmail.com>
2022-08-21 22:27:54 +0500

    added Test state

--- app.py
+++ app.py
@@ -11,13 +11,14 @@ async def on_startup(dp):
     await on_startup_notify(dp)
 
 
-async def on_shutdown(dp):
-    await bot.close()
-    await storage.close()
+# async def on_shutdown(dp):
+#     await bot.close()
+#     await storage.close()
 
 
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
 
-    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    # executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    executor.start_polling(dp, on_startup=on_startup)
--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -4,4 +4,7 @@ from .admin import dp
 
 from .echo import dp
 
+# from .testing import dp
+
+
 __all__ = ['dp']
